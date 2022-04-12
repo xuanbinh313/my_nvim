9cb054889d987d95386d6cf58609bca5f57b9211
xuanbinh313 <xuanbinh313@gmail.com>
2022-04-12 16:23:54 +0700

    change theme

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -1,4 +1,19 @@
 -- require('nightfox').load('duskfox')
-vim.cmd('colorscheme gruvbox')
+
+-- them gruvbox
+--vim.cmd('colorscheme gruvbox')
 -- vim.g.colorscheme = "gruvbox"
-vim.o.background = "dark"
+
+-- them onedark
+require('onedark').setup {
+  colors = {
+    bright_orange = "#ff8800",    -- define a new color
+    green = '#00ffaa',            -- redefine an existing color
+  },
+  highlights = {
+    TSKeyword = {fg = '$green'},
+    TSString = {fg = '$bright_orange',  fmt = 'bold'},
+    TSFunction = {fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic'},
+  }
+}
+require('onedark').load()
